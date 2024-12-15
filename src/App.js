1acc838b15d501b9435d2285944bf302f115ec8d
NamanTandon <namantandon16@gmail.com>
2024-12-15 16:50:05 +0530

    ContextAPI Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Naman Is Best React Developer and is rising star in Telstra.
+    </div>
+  );
+}
+
+export default App;
